76ebef73b598a2ee850417aefe31ed0d84ebbb9e
Mahek Dave <contact.mahekdave@gmail.com>
2025-10-09 19:56:00 +0530

    Update drop-to-bottom.js

--- hrc-site/drop-to-bottom.js
+++ hrc-site/drop-to-bottom.js
@@ -1,10 +1,10 @@
 const dropToBottomFn = (wrap, target = ".bg_circle") => {
+  const sec = document.querySelector(wrap);
   ScrollTrigger.create({
     trigger: sec,
     start: "top 80%", //
     end: "bottom top",
     onEnter: () => {
-      const sec = document.querySelector(wrap);
       const secRect = sec.getBoundingClientRect();
       const circlesArr = gsap.utils.toArray(sec.querySelectorAll(target));
       gsap.to(circlesArr, {
