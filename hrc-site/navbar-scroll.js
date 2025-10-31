463749c6cb13014b9050b7eac360f56cae7c8088
Mahek Dave <contact.mahekdave@gmail.com>
2025-10-31 20:53:34 +0530

    Create navbar-scroll.js

---
+++ hrc-site/navbar-scroll.js
@@ -0,0 +1,34 @@
+const createNavScroll = () => {
+let wasScrolling = false;
+const nav = document.querySelector('.c_navbar');
+
+const showNav = () => {
+  nav.classList.remove('nav--hidden');
+  nav.classList.add('nav--visible');
+}
+
+const hideNav = () => {
+  nav.classList.remove('nav--visible');
+  nav.classList.add('nav--hidden');
+}
+
+ScrollTrigger.create({
+	trigger: 'bosy',
+	start: 'top center',
+	end: 'bottom 50%+=100px',
+  marker: true
+});
+
+setInterval(()=>{
+const isScrolling = ScrollTrigger.isScrolling();
+
+  if (isScrolling && !wasScrolling) {
+     hideNav()
+  } else if (!isScrolling && wasScrolling) {
+    showNav();
+  }
+  wasScrolling = isScrolling;
+}, 100);
+};
+
+createNavScroll ();
