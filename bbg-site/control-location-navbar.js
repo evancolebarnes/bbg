3f5ee6ba282603aadd8076ad23e46a098a8652f1
Mahek Dave <contact.mahekdave@gmail.com>
2025-11-30 19:43:59 +0530

    Update control-location-navbar.js

--- bbg-site/control-location-navbar.js
+++ bbg-site/control-location-navbar.js
@@ -1,40 +1,41 @@
-const showNav = (nav) => {
-  nav.classList.remove("display-none");
+const showEl = (elArr) => {
+  elArr.forEach((el) => {
+    el.classList.remove("display-none");
+  });
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  const defaultNav = document.querySelector(".nav_default");
-  const locationNav = document.querySelector(".nav_location");
+  const defaultEl = [...document.querySelectorAll(".el_default")];
+  const locationEl = [...document.querySelectorAll(".el_location")];
 
   const loc = localStorage.getItem("userLocation");
 
   if (!loc) {
     // no location stored, use default nav
-    showNav(defaultNav);
+    showEl(defaultEl);
     return;
   }
 
-  const inputs = [...document.querySelectorAll("[data-location-input]")];
-  if (!(inputs.length > 0)) {
+  const input1 = document.querySelector("[data-location-input-footer]");
+  const input2 = document.querySelector("[data-location-input-navbar]");
+  if (!input1 || !input2) {
     // safety fallback
-    showNav(defaultNav);
+    showEl(defaultEl);
     return;
   }
 
   window.fsAttributes = window.fsAttributes || [];
-  //   inputs.forEach(i=>{i.value = loc});
+  input1.value = loc;
+  input2.value = loc;
   window.fsAttributes.push([
     "cmsfilter",
     (filterInstances) => {
       console.log("cmsfilter Successfully loaded!");
-      inputs.forEach((i) => {
-        i.value = loc;
-        i.dispatchEvent(new Event("input", { bubbles: true }));
-      });
+      input1.dispatchEvent(new Event("input", { bubbles: true }));
+      input2.dispatchEvent(new Event("input", { bubbles: true }));
 
       // The callback passes a `filterInstances` array with all the `CMSFilters` instances on the page.
       const [filterInstance] = filterInstances;
-      a;
       // The `renderitems` event runs whenever the list renders items after filtering.
       filterInstance.listInstance.on("renderitems", (renderedItems) => {
         console.log(renderedItems);
@@ -43,6 +44,6 @@ document.addEventListener("DOMContentLoaded", () => {
   ]);
 
   setTimeout(() => {
-    showNav(locationNav);
+    showEl(locationEl);
   }, 500);
 });
