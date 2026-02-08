c466598872d31d49bd024d2702bfbac090ba7f72
Mahek Dave <contact.mahekdave@gmail.com>
2026-02-08 22:26:10 +0530

    Create foundation-animation.js

---
+++ rbh-site/foundation-animation.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", () => {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".foundation_track",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true,
+      pin: true,
+      markers: true,
+      anticipatePin: 1,
+    },
+  });
+
+  tl.from("[data-anim-custom = reveal-from-below]", {
+    y: 60,
+    opacity: 0,
+    stagger: 0.5,
+    duration: 2,
+  })
+    .from(
+      "[data-anim-custom = reveal-from-above]",
+      { y: -60, opacity: 0, duration: 2, stagger: 0.5 },
+      "<1",
+    )
+    .to("[data-section-identifier=foundation-top]", {
+      clipPath: "polygon(0 0,0 100%,0% 100%,0% 0)",
+      duration: 2,
+    });
+});
