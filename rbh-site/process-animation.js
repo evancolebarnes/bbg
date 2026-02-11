8437ec08800d3ab78e838b03bf883aff254fa72e
Mahek Dave <contact.mahekdave@gmail.com>
2026-02-11 15:08:36 +0530

    Create process-animation.js

---
+++ rbh-site/process-animation.js
@@ -0,0 +1,62 @@
+const setupProcessAnimation = () =>{
+  const allImages = document
+  .querySelector(".process_track")
+  .querySelectorAll(".u-image-wrapper");
+const allParaWrap = document.querySelectorAll(".process_para_wrap");
+gsap.set(allImages, { opacity: 0 });
+gsap.set(allImages[allImages.length - 1], { opacity: 1 });
+
+gsap.set(allParaWrap, { height: 0, overflow: "hidden" });
+gsap.set(allParaWrap[0], {
+  height: "auto",
+  overflow: "visible",
+});
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".process_track",
+    start: "top top",
+    end: "bottom bottom",
+    pin: true,
+    scrub: true,
+    markers: true,
+    anticipatePin: 1,
+  },
+});
+const imagesReversed = [...allImages].reverse();
+
+imagesReversed.forEach((image, i) => {
+  const currentWrap = allParaWrap[i];
+  const prevWrap = allParaWrap[i - 1];
+  const text = currentWrap.querySelector(".process_block_text");
+
+  gsap.set(text, { yPercent: 100, opacity: 0 });
+
+  const step = gsap.timeline({
+    onComplete: () => {
+      gsap.to(text, {
+        yPercent: 0,
+        opacity: 1,
+        duration: 0.5,
+        ease: "power2.out",
+      });
+    },
+  });
+
+  step
+    .to(image, { opacity: 1 }, 0)
+    .to(currentWrap, {
+      height: "auto",
+    })
+    .to(
+      prevWrap || {},
+      {
+        height: 0,
+        overflow: "hidden",
+      },
+      "<",
+    );
+
+  tl.add(step);
+});
+}
