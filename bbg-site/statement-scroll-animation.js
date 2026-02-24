b85d5d6cf27929552cf5fcddcb06759c04aacf39
Mahek Dave <contact.mahekdave@gmail.com>
2026-02-24 17:02:19 +0530

    Create statement-scroll-animation.js

---
+++ bbg-site/statement-scroll-animation.js
@@ -0,0 +1,26 @@
+const setupStatementAnimation = () => {
+  const frame = document.querySelector(".statement_track");
+  if (!frame) return;
+
+
+  gsap.set(frame, { height: "500vh" });
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: frame,
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true,
+      pin: true,
+      anticipatePin: 1,
+      markers: true,
+      invalidateOnRefresh: true,
+    },
+  });
+
+  tl.to("[data-identifier='statement-top-sec']", {
+    clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)",
+    ease: "none",
+    duration: 1,
+  });
+};
