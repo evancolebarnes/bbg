6adb3fe839aef560a456682669651c0ecd8f4ecb
Mahek Dave <contact.mahekdave@gmail.com>
2026-05-04 17:17:37 +0530

    Create cta-move-over-element.js

---
+++ pure-site/codes/cta-move-over-element.js
@@ -0,0 +1,17 @@
+const ctaMoveOverElement = (wrapEl, ctaEl) => {
+  const wraps = document.querySelectorAll(wrapEl);
+  wraps.forEach((wrap) => {
+    const button = wrap.querySelector(".button_main_wrap");
+    const buttonRect = button.getBoundingClientRect();
+
+    wrap.addEventListener("mousemove", (event) => {
+      const wrapRect = wrap.getBoundingClientRect();
+
+      const x = event.clientX - wrapRect.left;
+      const y = event.clientY - wrapRect.top;
+
+      button.style.left = `${x - buttonRect.width / 2}px`;
+      button.style.top = `${y - buttonRect.height / 2}px`;
+    });
+  });
+};
