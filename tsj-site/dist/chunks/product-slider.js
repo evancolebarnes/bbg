13bb3dc6a14fa4c4a823c874b0367fcd40733852
mahek-developer <contact.mahekdave@gmail.com>
2026-07-29 19:11:35 +0530

    made some changes in the slider functionality

--- tsj-site/dist/chunks/product-slider.js
+++ tsj-site/dist/chunks/product-slider.js
@@ -4,28 +4,21 @@ function t() {
 	let e = document.querySelector(".products_list_wrap"), t = document.querySelector(".products_list"), n = [...document.querySelectorAll(".products_item")];
 	if (!e || !t || !n.length) return;
 	let r = null;
-	function i() {
-		if (window.innerWidth >= 991) {
-			r &&= (r.destroy(!0, !0), null), e.classList.remove("swiper"), t.classList.remove("swiper-wrapper"), n.forEach((e) => {
-				e.classList.remove("swiper-slide"), e.removeAttribute("style");
-			}), t.removeAttribute("style"), e.removeAttribute("style");
-			return;
+	r ||= (e.classList.add("swiper"), t.classList.add("swiper-wrapper"), n.forEach((e) => e.classList.add("swiper-slide")), new Swiper(e, {
+		slidesPerView: 1,
+		spaceBetween: 20,
+		breakpoints: { 480: {
+			slidesPerView: 2,
+			spaceBetween: 20
+		} },
+		navigation: {
+			nextEl: ".products_slider_button.is-next-button",
+			prevEl: ".products_slider_button.is-prev-button"
 		}
-		r ||= (e.classList.add("swiper"), t.classList.add("swiper-wrapper"), n.forEach((e) => e.classList.add("swiper-slide")), new Swiper(e, {
-			slidesPerView: 1,
-			spaceBetween: 20,
-			breakpoints: { 480: {
-				slidesPerView: 2,
-				spaceBetween: 20
-			} },
-			navigation: {
-				nextEl: ".products_slider_button.is-next-button",
-				prevEl: ".products_slider_button.is-prev-button"
-			}
-		}));
-	}
-	i(), window.addEventListener("resize", i);
+	}));
 }
 //#endregion
-e((() => {}))();
+e((() => {
+	initSwiper(), window.addEventListener("resize", initSwiper);
+}))();
 export { t as default };
--- tsj-site/src/functionality/product-slider.js
+++ tsj-site/src/functionality/product-slider.js
@@ -10,47 +10,45 @@ export default function productSlider() {
   function initSwiper() {
     const isDesktop = window.innerWidth >= 991;
 
-    if (isDesktop) {
-      if (swiperInstance) {
-        swiperInstance.destroy(true, true);
-        swiperInstance = null;
-      }
-
-      listWrap.classList.remove("swiper");
-      list.classList.remove("swiper-wrapper");
-      items.forEach((item) => {
-        item.classList.remove("swiper-slide");
-        item.removeAttribute("style");
-      });
-
-      list.removeAttribute("style");
-      listWrap.removeAttribute("style");
-
-      return;
+    if (swiperInstance) {
+      swiperInstance.destroy(true, true);
+      swiperInstance = null;
     }
 
-    if (swiperInstance) return;
-
-    listWrap.classList.add("swiper");
-    list.classList.add("swiper-wrapper");
-    items.forEach((item) => item.classList.add("swiper-slide"));
-
-    swiperInstance = new Swiper(listWrap, {
-      slidesPerView: 1,
-      spaceBetween: 20,
-      breakpoints: {
-        480: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-      },
-      navigation: {
-        nextEl: ".products_slider_button.is-next-button",
-        prevEl: ".products_slider_button.is-prev-button",
-      },
+    listWrap.classList.remove("swiper");
+    list.classList.remove("swiper-wrapper");
+    items.forEach((item) => {
+      item.classList.remove("swiper-slide");
+      item.removeAttribute("style");
     });
+
+    list.removeAttribute("style");
+    listWrap.removeAttribute("style");
+
+    return;
   }
 
-  initSwiper();
-  window.addEventListener("resize", initSwiper);
+  if (swiperInstance) return;
+
+  listWrap.classList.add("swiper");
+  list.classList.add("swiper-wrapper");
+  items.forEach((item) => item.classList.add("swiper-slide"));
+
+  swiperInstance = new Swiper(listWrap, {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+    },
+    navigation: {
+      nextEl: ".products_slider_button.is-next-button",
+      prevEl: ".products_slider_button.is-prev-button",
+    },
+  });
 }
+
+initSwiper();
+window.addEventListener("resize", initSwiper);
