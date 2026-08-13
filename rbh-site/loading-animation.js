40ba92a6eef9aae0a22cc6230c7f00f9e7659b9c
Mahek Dave <contact.mahekdave@gmail.com>
2026-08-13 16:40:44 +0530

    Update loading-animation.js

--- rbh-site/loading-animation.js
+++ rbh-site/loading-animation.js
@@ -1,4 +1,164 @@
 const initLoadingAnimation = (callBack) => {
+  gsap.set(".loading_column", { y: 0 });
+  gsap.set("body", {
+    height: "100vh",
+    overflow: "hidden",
+  });
+
+  const mm = gsap.matchMedia();
+
+  // =========================
+  // DESKTOP
+  // =========================
+
+  mm.add("(min-width: 768px)", () => {
+    const tl = gsap.timeline({
+      defaults: {
+        ease: "power2.out",
+      },
+    });
+
+    tl.to(".loading_image:not([data-logo])", {
+      opacity: 1,
+    });
+
+    tl.to(
+      ".loading_column.is-left, .loading_column.is-right",
+      {
+        y: 100,
+      },
+      0,
+    );
+
+    tl.to(
+      ".loading_column.is-middle",
+      {
+        y: -100,
+      },
+      0,
+    );
+
+    tl.to(".loading_image:not([data-hero-sec])", {
+      opacity: 0,
+    });
+
+    tl.to(".loading_image:not([data-hero-sec])", {
+      width: 0,
+      height: 0,
+      marginBottom: 0,
+    });
+
+    tl.to(
+      "[data-hero-sec]",
+      {
+        marginBottom: 0,
+      },
+      "<",
+    );
+
+    tl.to(
+      ".loading_column.is-left, .loading_column.is-right",
+      {
+        width: 0,
+        padding: 0,
+      },
+      "<",
+    );
+
+    tl.set(".loading_column.is-middle .loading_image:not([data-hero-sec])", {
+      display: "none",
+    });
+
+    tl.to(".loading_frame", {
+      height: "auto",
+    });
+
+    tl.to(
+      ".loading_column.is-middle",
+      {
+        y: 0,
+      },
+      "<",
+    );
+
+    tl.set(".loading_column.is-left, .loading_column.is-right", {
+      display: "none",
+    });
+
+    tl.set("body", {
+      height: "auto",
+      overflow: "auto",
+    });
+  });
+
+  // =========================
+  // MOBILE
+  // =========================
+
+  mm.add("(max-width: 767px)", () => {
+    const tl = gsap.timeline({
+      defaults: {
+        ease: "power2.out",
+      },
+    });
+
+    tl.to(".loading_image:not([data-logo])", {
+      opacity: 1,
+    });
+
+    tl.to(".loading_column.is-middle", {
+      y: -100,
+    });
+
+    tl.to(".loading_image:not([data-hero-sec])", {
+      opacity: 0,
+    });
+
+    tl.to(".loading_image:not([data-hero-sec])", {
+      width: 0,
+      height: 0,
+      marginBottom: 0,
+    });
+
+    tl.to(
+      "[data-hero-sec]",
+      {
+        marginBottom: 0,
+      },
+      "<",
+    );
+
+    tl.set(".loading_column.is-middle .loading_image:not([data-hero-sec])", {
+      display: "none",
+    });
+
+    tl.to(
+      ".loading_column.is-middle",
+      {
+        padding: 0,
+        y: 0,
+      },
+      "<",
+    );
+
+    tl.to(".loading_frame", {
+      height: "auto",
+    });
+
+    tl.set(
+      "body",
+      {
+        height: "auto",
+        overflow: "auto",
+      },
+      "<",
+    );
+  });
+};
+
+
+/*
+const initLoadingAnimation = (callBack) => {
   const bg = document
     .querySelector(".loading_frame")
     .querySelector(".c-background");
@@ -56,3 +216,4 @@ const initLoadingAnimation = (callBack) => {
     if (callBack) callBack();
   });
 };
+*/
